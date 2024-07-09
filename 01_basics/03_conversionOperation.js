3268c1a1b79b3128d36fd6a49fb48aebce9c741d
nehaschaudhari95 <nehaschaudhari95@gmail.com>
2024-07-09 15:17:15 +0530

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,58 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let newNum = "33"
+
+// let newN= Number(newNum)
+// console.log(typeof newN);
+// console.log(newN);
+
+let nemString = "33abc"
+// let newString = Number(nemString)
+// console.log(typeof newString);
+// console.log(newString);
+
+let newScore = true
+// let newS = Number(newScore);
+// console.log(typeof newS);
+// console.log(newS);
+
+let isLoggedIn = "Neha"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 77
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
